2b1c7aede1aadc081f9ad329d0c5f252e16dab0f
Maneesha kondraveedi <maneeshakcse@gmail.com>
2026-05-23 22:01:02 +0530

    Member 3: Added RMSProp optimizer

--- src/optimizers.py
+++ src/optimizers.py
@@ -5,30 +5,42 @@ class OptimizerEngine:
         self.optimizer_type = optimizer_type
         self.lr = learning_rate
         self.epochs = epochs
-        self.velocity = None
-        self.m = None
+        # state variables for optimizers
         self.v = None
-        self.t = None
+        self.m = None
+        self.t = 0
 
     def update_params(self, w, grad, epoch):
-        # Batch Gradient Descent
+        """Update weights based on optimizer type"""
         if self.optimizer_type == "batch":
             w = w - self.lr * grad
 
-        # Momentum Optimizer
         elif self.optimizer_type == "momentum":
-            if self.velocity is None:
-                self.velocity = np.zeros_like(w)
-            beta = 0.95   # momentum factor (try 0.95 also)
-            self.velocity = beta * self.velocity + self.lr * grad
-            w = w - self.velocity
+            if self.v is None:
+                self.v = np.zeros_like(w)
+            beta = 0.9
+            self.v = beta * self.v + (1 - beta) * grad
+            w = w - self.lr * self.v
 
-        # RMSProp placeholder
         elif self.optimizer_type == "rmsprop":
-            pass
+            if self.v is None:
+                self.v = np.zeros_like(w)
+            beta = 0.9
+            eps = 1e-8
+            self.v = beta * self.v + (1 - beta) * (grad ** 2)
+            w = w - (self.lr / (np.sqrt(self.v) + eps)) * grad
 
-        # Adam placeholder
         elif self.optimizer_type == "adam":
-            pass
+            if self.m is None:
+                self.m = np.zeros_like(w)
+                self.v = np.zeros_like(w)
+                self.t = 0
+            beta1, beta2, eps = 0.9, 0.999, 1e-8
+            self.t += 1
+            self.m = beta1 * self.m + (1 - beta1) * grad
+            self.v = beta2 * self.v + (1 - beta2) * (grad ** 2)
+            m_hat = self.m / (1 - beta1 ** self.t)
+            v_hat = self.v / (1 - beta2 ** self.t)
+            w = w - (self.lr / (np.sqrt(v_hat) + eps)) * m_hat
 
         return w
