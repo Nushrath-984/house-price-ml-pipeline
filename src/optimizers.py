e4622495406b4a4bdcc1259633d31fd910649062
Nushrath <nushrathkallu@gmail.com>
2026-05-23 23:56:27 +0530

    Unified optimizer engine and regenerated all results

--- src/optimizers.py
+++ src/optimizers.py
@@ -5,31 +5,35 @@ class OptimizerEngine:
         self.optimizer_type = optimizer_type
         self.lr = learning_rate
         self.epochs = epochs
-        # state variables for optimizers
-        self.v = None
-        self.m = None
-        self.t = 0
+        self.velocity = None   # for Momentum
+        self.s = None          # for RMSProp
+        self.m = None          # for Adam
+        self.v = None          # for Adam
+        self.t = 0             # timestep for Adam
 
     def update_params(self, w, grad, epoch):
-        """Update weights based on optimizer type"""
+        # Member 1 – Batch GD
         if self.optimizer_type == "batch":
             w = w - self.lr * grad
 
+        # Member 2 – Momentum
         elif self.optimizer_type == "momentum":
-            if self.v is None:
-                self.v = np.zeros_like(w)
-            beta = 0.9
-            self.v = beta * self.v + (1 - beta) * grad
-            w = w - self.lr * self.v
+            if self.velocity is None:
+                self.velocity = np.zeros_like(w)
+            beta = 0.9   # or 0.95 for second run
+            self.velocity = beta * self.velocity + self.lr * grad
+            w = w - self.velocity
 
+        # Member 3 – RMSProp
         elif self.optimizer_type == "rmsprop":
-            if self.v is None:
-                self.v = np.zeros_like(w)
+            if self.s is None:
+                self.s = np.zeros_like(w)
             beta = 0.9
             eps = 1e-8
-            self.v = beta * self.v + (1 - beta) * (grad ** 2)
-            w = w - (self.lr / (np.sqrt(self.v) + eps)) * grad
+            self.s = beta * self.s + (1 - beta) * (grad ** 2)
+            w = w - (self.lr / (np.sqrt(self.s) + eps)) * grad
 
+        # Member 4 – Adam
         elif self.optimizer_type == "adam":
             if self.m is None:
                 self.m = np.zeros_like(w)
